c21e90d8f18b78f89f8eba20eeca4dc31db7c914
Matheus Laurindo <matheus104521@gmail.com>
2024-02-06 20:19:14 -0300

    feat: instala logos

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,5 @@
+import logo from '@assets/logo-nlw-expert.svg';
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <img src={logo} alt="" />;
 }
